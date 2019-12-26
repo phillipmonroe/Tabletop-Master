3bd033cf1977e426896357458074c786ad2837a2
wesquintana <wesquintana@gmail.com>
2019-12-26 15:29:17 -0700

    implements #5

---
+++ client/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login container-fluid">
+    <form @submit.prevent="login">
+      <input type="text" placeholder="Enter your name..." />
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "login",
+  components: {}
+};
+</script>
